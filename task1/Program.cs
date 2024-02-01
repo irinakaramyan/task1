22d594a5214785c5e9dd373ac875ff9d241569d1
Irina <Irina@DESKTOP-25PA27Q>
2024-02-01 17:25:09 +0400

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = new int[8, 8];
+
+        // Define horse strokes
+        int[] dx = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int[] dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+        
+        int x = 3; // for example
+        int y = 3; // for example
+
+       
+        for (int i = 0; i < 8; i++)
+        {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8)
+            {
+                matrix[newX, newY] = 1; // 1 can represent the visited position of the horse
+            }
+        }
+
+        
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
